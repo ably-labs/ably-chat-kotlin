ba0f81e7f5aa42d507e1e527b57010b31f42e6e1
sacOO7 <sachinshinde7676@gmail.com>
2024-10-16 17:08:46 +0530

    Implemented missing interfaces as a part of RoomLifeCycleManager

--- chat-android/src/main/java/com/ably/chat/RoomLifecycleManager.kt
+++ chat-android/src/main/java/com/ably/chat/RoomLifecycleManager.kt
@@ -1,16 +1,18 @@
 package com.ably.chat
 
 import io.ably.lib.realtime.ChannelBase
+import io.ably.lib.types.ErrorInfo
+import kotlinx.coroutines.Deferred
 
 /**
  * An interface for features that contribute to the room status.
  */
-interface ContributesToRoomLifecycle: Subscription {
+interface ContributesToRoomLifecycle: EmitsDiscontinuities {
     /**
      * Gets the channel on which the feature operates. This promise is never
      * rejected except in the case where room initialization is canceled.
      */
-    val channel: ChannelBase
+    val channel: Deferred<ChannelBase>
 
     /**
      * Gets the ErrorInfo code that should be used when the feature fails to attach.
@@ -26,6 +28,18 @@ interface ContributesToRoomLifecycle: Subscription {
 }
 
 /**
+ * This interface represents a feature that contributes to the room lifecycle and
+ * exposes its channel directly. Objects of this type are created by awaiting the
+ * channel promises of all the {@link ContributesToRoomLifecycle} objects.
+ *
+ * @internal
+ */
+interface ResolvedContributor {
+    val channel: ChannelBase
+    val contributor: ContributesToRoomLifecycle
+}
+
+/**
  * The order of precedence for lifecycle operations, passed to PriorityQueueExecutor which allows
  * us to ensure that internal operations take precedence over user-driven operations.
  */
@@ -35,6 +49,22 @@ enum class LifecycleOperationPrecedence(val operationPriority: Int) {
     AttachOrDetach(3),
 }
 
+/**
+ * A map of contributors to pending discontinuity events.
+ */
+typealias DiscontinuityEventMap = Map<ResolvedContributor, ErrorInfo?>
+
+/**
+ * An internal interface that represents the result of a room attachment operation.
+ */
+interface RoomAttachmentResult : NewRoomStatus {
+    val failedFeature: ResolvedContributor?
+}
+
+/**
+ * An implementation of the `Status` interface.
+ * @internal
+ */
 class RoomLifecycleManager {
 
 }
