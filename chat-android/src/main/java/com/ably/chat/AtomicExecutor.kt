e3856141bdd96e4a751fd3522ec100e475111641
sacOO7 <sachinshinde7676@gmail.com>
2024-10-17 17:00:20 +0530

    Refactored/improved while loop inside AtomicExecutor

--- chat-android/src/main/java/com/ably/chat/AtomicExecutor.kt
+++ chat-android/src/main/java/com/ably/chat/AtomicExecutor.kt
@@ -22,21 +22,23 @@ private class Task(
  */
 class AtomicExecutor(private val scope: CoroutineScope) {
     private var isRunning = false
-    private val priorityQueue: PriorityQueue<Task> = PriorityQueue()
+    private val tasks: PriorityQueue<Task> = PriorityQueue()
 
     suspend fun <T : Any>execute(priority:Int, coroutineBlock: suspend CoroutineScope.() -> T) : Channel<Result<T>> {
         // Size of resultChannel is set to 1 to keep while loop running.
         // i.e. If caller doesn't explicitly receive on the channel, loop will be blocked.
         val resultChannel = Channel<Result<Any>>(1)
-        priorityQueue.add(Task(priority, coroutineBlock, resultChannel))
+        tasks.add(Task(priority, coroutineBlock, resultChannel))
 
         scope.launch {
             if (!isRunning) {
                 isRunning = true
-                while (priorityQueue.size > 0) {
-                    val task = priorityQueue.remove()
-                    val result = kotlin.runCatching { scope.async(block = task.coroutineBlock).await() }
-                    task.resultChannel.send(result)
+                while (tasks.isNotEmpty()) {
+                    val task = tasks.poll()
+                    task?.let {
+                        val result = kotlin.runCatching { scope.async(block = it.coroutineBlock).await() }
+                        it.resultChannel.send(result)
+                    }
                 }
                 isRunning = false
             }
