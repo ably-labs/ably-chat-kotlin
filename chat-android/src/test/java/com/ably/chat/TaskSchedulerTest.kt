98478865278dc14c073beb1d1e65fac811137ca7
sacOO7 <sachinshinde7676@gmail.com>
2024-10-18 16:29:20 +0530

    Added test to check if ThreadScheduler captures failure of the operation

--- chat-android/src/test/java/com/ably/chat/TaskSchedulerTest.kt
+++ chat-android/src/test/java/com/ably/chat/TaskSchedulerTest.kt
@@ -1,5 +1,7 @@
 package com.ably.chat
 
+import io.ably.lib.types.AblyException
+import io.ably.lib.types.ErrorInfo
 import java.util.concurrent.Executors
 import kotlin.time.DurationUnit
 import kotlin.time.toDuration
@@ -27,15 +29,32 @@ class TaskSchedulerTest {
     }
 
     @Test
+    fun `should capture failure of the given operation`() = runTest {
+        val singleThreadedDispatcher = Executors.newSingleThreadExecutor().asCoroutineDispatcher()
+        val taskScheduler = TaskScheduler(CoroutineScope(singleThreadedDispatcher))
+        val taskResult = taskScheduler.schedule {
+            delay(2000)
+            throw AblyException.fromErrorInfo(ErrorInfo("Error performing operation", 400))
+        }
+        val result = taskResult.await()
+        Assert.assertFalse(result.isSuccess)
+        Assert.assertTrue(result.isFailure)
+        val exception = result.exceptionOrNull() as AblyException
+        Assert.assertEquals("Error performing operation", exception.errorInfo.message)
+    }
+
+    @Test
     fun `should perform mutually exclusive operations with given priority`() = runTest {
         val singleThreadedDispatcher = Executors.newSingleThreadExecutor().asCoroutineDispatcher()
         val taskScheduler = TaskScheduler(CoroutineScope(singleThreadedDispatcher))
         val taskResults = mutableListOf<TaskResult<Int>>()
         var operationInProgress = false
         var counter = 0
+        val threadIds = mutableSetOf<Long>()
 
         repeat(20) {
             val result = taskScheduler.schedule(it) {
+                threadIds.add(Thread.currentThread().id)
                 if (operationInProgress) {
                     throw IllegalStateException("Can't perform operation when other operation is going on")
                 }
@@ -48,10 +67,12 @@ class TaskSchedulerTest {
         }
 
         val results = taskResults.map { it.await() }
-
         repeat(20) {
             Assert.assertTrue(results[it].isSuccess)
             Assert.assertEquals(it, results[it].getOrNull())
         }
+
+        // Scheduler should run all async operations under single thread
+        Assert.assertEquals(1, threadIds.size);
     }
 }
