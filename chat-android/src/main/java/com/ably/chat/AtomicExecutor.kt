31ca6e3cf5720150aa886697b28fbfed855be435
sacOO7 <sachinshinde7676@gmail.com>
2024-10-18 12:39:15 +0530

    Updated AtomicExecutor, set default value for task priority, added documentation for the same

--- chat-android/src/main/java/com/ably/chat/AtomicExecutor.kt
+++ chat-android/src/main/java/com/ably/chat/AtomicExecutor.kt
@@ -24,12 +24,17 @@ class AtomicExecutor(private val scope: CoroutineScope) {
     private var isRunning = false
     private val tasks: PriorityQueue<Task> = PriorityQueue()
 
-    suspend fun <T : Any>execute(priority:Int, coroutineBlock: suspend CoroutineScope.() -> T) : Channel<Result<T>> {
+    /**
+     * @param priority Defines priority for the operation execution.
+     *                 Default=0, means operation will be performed immediately after ongoing operation.
+     *                 This can also be set to negative number if operation needs higher priority than existing ones.
+     * @param coroutineBlock Suspended function that needs to be executed mutually exclusive under given scope.
+     */
+    suspend fun <T : Any>execute(priority:Int = 0, coroutineBlock: suspend CoroutineScope.() -> T) : Channel<Result<T>> {
         // Size of resultChannel is set to 1 to keep while loop running.
         // i.e. If caller doesn't explicitly receive on the channel, loop will be blocked.
         val resultChannel = Channel<Result<Any>>(1)
         tasks.add(Task(priority, coroutineBlock, resultChannel))
-
         scope.launch {
             if (!isRunning) {
                 isRunning = true
