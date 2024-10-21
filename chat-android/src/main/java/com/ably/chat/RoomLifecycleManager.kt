a68a2a7c6849e7ce71d17d04a189976e9efec488
sacOO7 <sachinshinde7676@gmail.com>
2024-10-21 18:53:49 +0530

    Implemented basic definition for RoomLifeCycleManager

--- chat-android/src/main/java/com/ably/chat/RoomLifecycleManager.kt
+++ chat-android/src/main/java/com/ably/chat/RoomLifecycleManager.kt
@@ -1,8 +1,11 @@
 package com.ably.chat
 
-import io.ably.lib.realtime.ChannelBase
+import io.ably.lib.realtime.Channel as AblyRealtimeChannel
 import io.ably.lib.types.ErrorInfo
+import io.ably.lib.util.Log.LogHandler
 import kotlinx.coroutines.CompletableDeferred
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
 
 /**
  * An interface for features that contribute to the room status.
@@ -12,7 +15,7 @@ interface ContributesToRoomLifecycle : EmitsDiscontinuities {
      * Gets the channel on which the feature operates. This promise is never
      * rejected except in the case where room initialization is canceled.
      */
-    val channel: CompletableDeferred<ChannelBase>
+    val channel: CompletableDeferred<AblyRealtimeChannel>
 
     /**
      * Gets the ErrorInfo code that should be used when the feature fails to attach.
@@ -35,7 +38,7 @@ interface ContributesToRoomLifecycle : EmitsDiscontinuities {
  * @internal
  */
 interface ResolvedContributor {
-    val channel: ChannelBase
+    val channel: AblyRealtimeChannel
     val contributor: ContributesToRoomLifecycle
 }
 
@@ -66,3 +69,51 @@ interface RoomAttachmentResult : NewRoomStatus {
  * @internal
  */
 class RoomLifecycleManager
+    (status: DefaultStatus, contributors: List<ResolvedContributor>, logger: LogHandler) {
+
+    /**
+     * The status of the room.
+     */
+    private var _status: DefaultStatus = status
+
+    /**
+     * The features that contribute to the room status.
+     */
+    private var _contributors: List<ResolvedContributor> = contributors
+
+    /**
+     * Logger for RoomLifeCycleManager
+     */
+    private val _logger: LogHandler = logger
+
+    /**
+     * sequentialCoroutineScope is to ensure the integrity and atomicity of operations that affect the room status, such as
+     * attaching, detaching, and releasing the room. It makes sure that we don't have multiple operations happening
+     * at once which could leave us in an inconsistent state.
+     * It is used as a CoroutineContext for with [kotlinx.coroutines.selects.select] statement.
+     * See [Kotlin Dispatchers](https://kt.academy/article/cc-dispatchers) for more information.
+     */
+    private val sequentialCoroutineScope = CoroutineScope(Dispatchers.Default.limitedParallelism(1))
+
+    /**
+     * This flag indicates whether some sort of controlled operation is in progress (e.g. attaching, detaching, releasing).
+     *
+     * It is used to prevent the room status from being changed by individual channel state changes and ignore
+     * underlying channel events until we reach a consistent state.
+     */
+    private var _operationInProgress = false;
+
+    /**
+     * A map of contributors to whether their first attach has completed.
+     *
+     * Used to control whether we should trigger discontinuity events.
+     */
+    private val _firstAttachesCompleted = mutableMapOf<ResolvedContributor, Boolean>()
+
+    init {
+        if (_status.current != RoomLifecycle.Attached) {
+            _operationInProgress = true
+        }
+        // TODO - [CHA-RL4] set up room monitoring here
+    }
+}
