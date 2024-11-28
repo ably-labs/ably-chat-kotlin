23734d42d73ef831c8f99cf0653973118ea60872
evgeny <khokhlov.e.n@gmail.com>
2024-11-28 11:31:56 +0000

    feat: add coverage checks with min 80% threshold

--- chat-android/build.gradle.kts
+++ chat-android/build.gradle.kts
@@ -4,6 +4,7 @@ plugins {
     alias(libs.plugins.android.library)
     alias(libs.plugins.android.kotlin)
     alias(libs.plugins.build.config)
+    alias(libs.plugins.kover)
 }
 
 val version = libs.versions.ably.chat.get()
